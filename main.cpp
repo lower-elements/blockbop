ef6915781660e45158b8c7c6d42a50b0e702740f
Michael Connor Buchan <mikey@blindcomputing.org>
2024-01-03 02:14:40 +0000

    Fix: Add OpenAL Soft as dependency

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
-#include <fmt/core.h>
-#include <flatbuffers/flatbuffers.h>
-#include <enet/enet.h>
-#include <SDL2pp/SDL2pp.hh>
 #include <SDL.h>
+#include <SDL2pp/SDL2pp.hh>
+#include <alc.h>
+#include <enet/enet.h>
+#include <flatbuffers/flatbuffers.h>
+#include <fmt/core.h>
 
 int main() {
-    fmt::print("ENet version: {:x}\n", enet_linked_version());
-    fmt::print("FlatBuffers version: {}\n", flatbuffers::flatbuffers_version_string());
-    fmt::print("SDL revision: {}\n", SDL_GetRevision());
-    fmt::print("SDL2pp version: {}\n", SDL2PP_VERSION);
-    return 0;
+  int openal_major, openal_minor;
+  alcGetIntegerv(nullptr, ALC_MAJOR_VERSION, 1, &openal_major);
+  alcGetIntegerv(nullptr, ALC_MINOR_VERSION, 1, &openal_minor);
+  fmt::print("ENet version: {:x}\n", enet_linked_version());
+  fmt::print("FlatBuffers version: {}\n",
+             flatbuffers::flatbuffers_version_string());
+  fmt::print("OpenAL version: {}.{}\n", openal_major, openal_minor);
+  fmt::print("SDL revision: {}\n", SDL_GetRevision());
+  fmt::print("SDL2pp version: {}\n", SDL2PP_VERSION);
+  return 0;
 }
