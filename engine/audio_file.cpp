a416138d4a0518f94f4abe5445b74acd429a6bf7
Michael Connor Buchan <mikey@blindcomputing.org>
2024-01-07 13:44:23 +0000

    Feat: Implement AudioFile class for loading buffers from files
    This will also facilitate streaming later on. We use libsndfile to
    decode the audio, and SDL's RWops to open and read files.

---
+++ engine/audio_file.cpp
@@ -0,0 +1,136 @@
+#include <cassert>
+#include <exception>
+#include <vector>
+
+#include <fmt/core.h>
+
+#include "audio_file.hpp"
+
+static sf_count_t rwops_get_filelen(void *data) {
+  auto ops = static_cast<SDL_RWops *>(data);
+  return (sf_count_t)SDL_RWsize(ops);
+}
+
+static sf_count_t rwops_seek(sf_count_t offset, int whence, void *data) {
+  auto ops = static_cast<SDL_RWops *>(data);
+  return (sf_count_t)SDL_RWseek(ops, offset, whence);
+}
+
+static sf_count_t rwops_read(void *ptr, sf_count_t count, void *data) {
+  auto ops = static_cast<SDL_RWops *>(data);
+  return (sf_count_t)SDL_RWread(ops, ptr, count, 1);
+}
+
+static sf_count_t rwops_write(const void *ptr, sf_count_t count, void *data) {
+  auto ops = static_cast<SDL_RWops *>(data);
+  return (sf_count_t)SDL_RWwrite(ops, ptr, count, 1);
+}
+
+static sf_count_t rwops_tell(void *data) {
+  auto ops = static_cast<SDL_RWops *>(data);
+  return (sf_count_t)SDL_RWtell(ops);
+}
+
+static struct SF_VIRTUAL_IO RWOPS_VIO = {
+    .get_filelen = rwops_get_filelen,
+    .seek = rwops_seek,
+    .read = rwops_read,
+    .write = rwops_write,
+    .tell = rwops_tell,
+};
+
+AudioFile::AudioFile(SDL2pp::RWops &&ops, int mode)
+    : m_ops(std::move(ops)), m_sndfile(nullptr) {
+  m_sndfile = sf_open_virtual(&RWOPS_VIO, mode, &m_info, m_ops.Get());
+  if (!m_sndfile) {
+    throw std::runtime_error(
+        fmt::format("Could not load audio file: {}", sf_strerror(nullptr)));
+  }
+}
+
+AudioFile AudioFile::fromFile(const std::string &path, int mode) {
+  std::string mode_str;
+  if (mode == SFM_READ) {
+    mode_str = "rb";
+  } else if (mode == SFM_WRITE) {
+    mode_str = "wb";
+  } else if (mode == SFM_RDWR) {
+    mode_str = "rb+";
+  } else {
+    throw std::runtime_error("Invalid mode specified when opening audio file");
+  }
+  auto ops(SDL2pp::RWops::FromFile(path, mode_str));
+  return AudioFile(std::move(ops), mode);
+}
+
+AudioFile::~AudioFile() {
+  if (m_sndfile) {
+    sf_close(m_sndfile);
+  }
+}
+
+sf_count_t AudioFile::read(void *ptr, sf_count_t bytes) {
+  auto res = sf_read_raw(m_sndfile, ptr, bytes);
+  if (res < 0) {
+    throw std::runtime_error(fmt::format("Could not read from audio file: {}",
+                                         sf_strerror(m_sndfile)));
+  }
+  return res;
+}
+
+static_assert(sizeof(short) == sizeof(std::int16_t));
+sf_count_t AudioFile::read(std::int16_t *ptr, sf_count_t bytes) {
+  auto res = sf_read_short(m_sndfile, ptr, bytes);
+  if (res < 0) {
+    throw std::runtime_error(fmt::format("Could not read from audio file: {}",
+                                         sf_strerror(m_sndfile)));
+  }
+  return res;
+}
+
+static_assert(sizeof(int) == sizeof(std::int32_t));
+sf_count_t AudioFile::read(std::int32_t *ptr, sf_count_t bytes) {
+  auto res = sf_read_int(m_sndfile, ptr, bytes);
+  if (res < 0) {
+    throw std::runtime_error(fmt::format("Could not read from audio file: {}",
+                                         sf_strerror(m_sndfile)));
+  }
+  return res;
+}
+
+sf_count_t AudioFile::read(float *ptr, sf_count_t bytes) {
+  auto res = sf_read_float(m_sndfile, ptr, bytes);
+  if (res < 0) {
+    throw std::runtime_error(fmt::format("Could not read from audio file: {}",
+                                         sf_strerror(m_sndfile)));
+  }
+  return res;
+}
+
+sf_count_t AudioFile::read(double *ptr, sf_count_t bytes) {
+  auto res = sf_read_double(m_sndfile, ptr, bytes);
+  if (res < 0) {
+    throw std::runtime_error(fmt::format("Could not read from audio file: {}",
+                                         sf_strerror(m_sndfile)));
+  }
+  return res;
+}
+
+openal::Buffer AudioFile::makeBuffer() {
+  auto num_samples = numSamples();
+  std::vector<std::int16_t> samples;
+  samples.reserve(num_samples);
+  auto num_read = read(samples.data(), num_samples);
+  samples.resize(num_read);
+  openal::Buffer buf;
+  if (m_info.channels == 1) {
+    buf.setMonoData(samples.data(), samples.size(), m_info.samplerate);
+  } else if (m_info.channels == 2) {
+    buf.setStereoData(samples.data(), samples.size(), m_info.samplerate);
+  } else {
+    throw std::runtime_error(
+        fmt::format("Invalid number of channels, expected 1 or 2, got {}",
+                    m_info.channels));
+  }
+  return buf;
+}
---
+++ engine/audio_file.hpp
@@ -0,0 +1,46 @@
+#ifndef BBENGINE_AUDIO_FILE_HPP
+#define BBENGINE_AUDIO_FILE_HPP
+
+#include <cstdint>
+
+#include <SDL2pp/RWops.hh>
+#include <sndfile.h>
+
+#include "al/buffer.hpp"
+
+class AudioFile {
+private: // Member methods
+  AudioFile(SDL2pp::RWops &&ops, int mode = SFM_READ);
+
+public:
+  static AudioFile fromFile(const std::string &path, int mode = SFM_READ);
+
+  // No copy
+  AudioFile(const AudioFile &) = delete;
+  AudioFile &operator=(const AudioFile &) = delete;
+
+  ~AudioFile();
+
+  sf_count_t numFrames() const { return m_info.frames; }
+  int getSampleRate() const { return m_info.samplerate; }
+  int numChannels() const { return m_info.channels; }
+  int getFormat() const { return m_info.format; }
+  int numSections() const { return m_info.sections; }
+  bool isSeekable() const { return m_info.seekable != 0; }
+  sf_count_t numSamples() const { return numFrames() * numChannels(); }
+
+  sf_count_t read(void *ptr, sf_count_t bytes);
+  sf_count_t read(std::int16_t *ptr, sf_count_t samples);
+  sf_count_t read(std::int32_t *ptr, sf_count_t samples);
+  sf_count_t read(float *ptr, sf_count_t samples);
+  sf_count_t read(double *ptr, sf_count_t samples);
+
+  openal::Buffer makeBuffer();
+
+private: // Member variables
+  SDL2pp::RWops m_ops;
+  SNDFILE *m_sndfile;
+  SF_INFO m_info;
+};
+
+#endif
