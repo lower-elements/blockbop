f0f3ec25c3df74292c56bb96f2cc20d27ebaf386
Michael Connor Buchan <mikey@blindcomputing.org>
2024-01-09 01:58:49 +0000

    Fix: Engine base-class member variables should be protected

--- engine/application.hpp
+++ engine/application.hpp
@@ -49,7 +49,7 @@ protected: // Functions implemented by subclasses
    */
   virtual void onUserDestroy();
 
-private: // Member variables
+protected: // Member variables
   SDL2pp::SDL m_sdl;
   SDL2pp::Window m_win;
   std::unique_ptr<tts::Speaker> m_speaker;
