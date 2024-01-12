4c51bd5cf6e7b1413e17b25e64394ae5632b46e6
Michael Connor Buchan <mikey@blindcomputing.org>
2024-01-12 18:47:31 +0000

    feat: Example main menu in Blockbop Application

--- client/blockbop.cpp
+++ client/blockbop.cpp
@@ -1,8 +1,11 @@
 #include "blockbop.hpp"
+#include "state/menu.hpp"
 
 Blockbop::Blockbop() : Application("BlockBop") {}
 
 bool Blockbop::onUserCreate() {
   m_speaker->speak("Welcome to BlockBop!");
+  m_states.push(
+      new Menu(*this, "Main Menu", {"Single Player", "Multi Player", "Exit"}));
   return true;
 }
