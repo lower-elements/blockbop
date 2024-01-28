84916c6df69a8ece6f9d38324d1f49ff423e397b
Michael Connor Buchan <mikey@blindcomputing.org>
2024-01-28 20:22:39 +0000

    feat: Replace main menu with text input for testing

--- client/blockbop.cpp
+++ client/blockbop.cpp
@@ -3,14 +3,17 @@
 #include "al/source.hpp"
 #include "blockbop.hpp"
 #include "state/menu.hpp"
+#include "state/text_input.hpp"
 
 Blockbop::Blockbop() : Application("lower-elements", "BlockBop") {}
 
 bool Blockbop::onUserCreate() {
   m_speaker->speak("Welcome to BlockBop!");
 
-  m_states.push(
-      new Menu(*this, "Main Menu", {"Single Player", "Multi Player", "Exit"}));
+  // m_states.push(
+  // new Menu(*this, "Main Menu", {"Single Player", "Multi Player", "Exit"}));
+
+  m_states.push(new TextInputState(*this, "Enter text: "));
 
   m_audio.streamByPath("complete.ogg");
 
