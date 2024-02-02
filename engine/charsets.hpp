f48bd708b3284f28e8c9430d434333538235c122
Michael Connor Buchan <mikey@blindcomputing.org>
2024-02-02 11:40:11 +0000

    feat: Add charset function for converting entire strings
    This uses SDL's existing SDL_iconv_string() function, and wraps the
    resulting allocated buffer in a unique_ptr.

--- engine/charsets.hpp
+++ engine/charsets.hpp
@@ -3,6 +3,9 @@
 
 #include <cstddef>
 
+#include <memory>
+#include <string_view>
+
 #include <SDL_stdinc.h>
 
 namespace charsets {
@@ -20,6 +23,26 @@ public: // Member methods
 private: // Member variables
   SDL_iconv_t m_cd;
 };
+
+template <class T = char>
+std::unique_ptr<T[], void (*)(void *)>
+convert(const char *tocode, const char *fromcode, const char *buf,
+        std::size_t size) {
+  T *str = (T *)SDL_iconv_string(tocode, fromcode, buf, size);
+  return {str, SDL_free};
+}
+
+template <class T = char>
+std::unique_ptr<T[], void (*)(void *)>
+convert(const char *tocode, const char *fromcode, std::string_view text) {
+  return convert<T>(tocode, fromcode, text.data(), text.size());
+}
+
+template <class T = char>
+std::unique_ptr<T[], void (*)(void *)>
+convert(const char *tocode, const char *fromcode, const std::string &text) {
+  return convert<T>(tocode, fromcode, text.data(), text.size());
+}
 
 } // namespace charsets
 
